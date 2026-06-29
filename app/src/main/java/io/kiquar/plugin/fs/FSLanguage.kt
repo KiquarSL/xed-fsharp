0465c24812a7746a890f03ff0525f63f612ad9b9
KiquarSL <io.dmitry.akimov@gmail.com>
2026-06-29 22:12:38 +0700

    Added extensions to FSLanguage

--- app/src/main/java/io/kiquar/plugin/fs/FSLanguage.kt
+++ app/src/main/java/io/kiquar/plugin/fs/FSLanguage.kt
@@ -5,7 +5,7 @@ import com.rk.file.FileType
 import com.rk.icons.Icon
 
 class FSLanguage(resources: Resources) : FileType {
-    override val extensions = listOf("fs")
+    override val extensions = listOf("fs", "fsi", "fsx")
     override val textmateScope = "source.fs"
     override val name = "fsharp"
     override val title = "F#"
